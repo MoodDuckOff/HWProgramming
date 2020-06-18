210a6326f736b8a37d4b22e2e136a3dbd4859171
Vitaliy Shatskiy <36418695+MoodDuckOff@users.noreply.github.com>
2020-06-18 20:19:38 +0300

    add authgard and role model

--- hwp-frontend/src/app/guards/auth.guard.ts
+++ hwp-frontend/src/app/guards/auth.guard.ts
@@ -14,6 +14,13 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const user = this.accountService.userValue;
     if (user) {
+      // check if route is restricted by role
+      if (route.data.roles && route.data.roles.indexOf(user.role) === -1) {
+        // role not authorised so redirect to home page
+        this.router.navigate(['/']);
+        return false;
+      }
+
       // authorised so return true
       return true;
     }
--- hwp-frontend/src/app/models/user.ts
+++ hwp-frontend/src/app/models/user.ts
@@ -1,8 +1,12 @@
-﻿export class User {
+﻿import { Role } from './role.model';
+
+
+export class User {
   id: string;
   username: string;
   password: string;
   firstName: string;
   lastName: string;
+  role: Role;
   token: string;
 }
